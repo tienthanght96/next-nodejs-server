bde2d3c399986fa44841740b9374c49b3ecf8f6a
ThangTran <thangtran@ThangTrans-MBP.net.fpt>
2019-03-06 23:59:17 +0700

    Update user authen, find all users

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,41 +1,86 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
+const passport = require('passport');
 
 exports.validateSignup = (req, res, next) => {
-  req.sanitizeBody('name');
-  req.sanitizeBody('email');
-  req.sanitizeBody('password');
+  req.sanitizeBody("name");
+  req.sanitizeBody("email");
+  req.sanitizeBody("password");
 
-  req.checkBody('name', 'Enter a name').notEmpty();
-  req.checkBody('name', 'Name must be between 4 and 10 character').isLength({min: 4, max: 10});
+  req.checkBody("name", "Enter a name").notEmpty();
+  req.checkBody("name", "Name must be between 4 and 10 characters")
+    .isLength({
+      min: 4,
+      max: 10
+    });
 
-  req.checkBody('email', 'Enter a valid email').isEmail().normalizeEmail();
+  req.checkBody("email", "Enter a valid email")
+    .isEmail().normalizeEmail();
 
-  req.checkBody('password', 'Enter a password').notEmpty();
-  req.checkBody('password', 'Password must be between 4 and 10 character').isLength({min: 4, max: 10});
+  req.checkBody("password", "Enter a password").notEmpty();
+  req
+    .checkBody("password", "Password must be between 4 and 10 characters")
+    .isLength({
+      min: 4,
+      max: 10
+    });
 
   const errors = req.validationErrors();
-  if(errors) {
+  if (errors) {
     const firstError = errors.map(error => error.msg)[0];
-    return res.status(400, firstError);
+    return res.status(400).send(firstError);
   }
   next();
+
 };
 
 exports.signup = async (req, res) => {
-  const { name, email, password } = req.body;
-  const user = await new User({ name, email, password }).save();
+  const {
+    name,
+    email,
+    password
+  } = req.body;
+  const user = await new User({
+    name,
+    email,
+    password
+  });
   await User.register(user, password, (error, user) => {
-    if(error) {
+    if (error) {
       return res.status(500).send(error.message);
     }
-    return res.json(user);
+    res.json(user);
   });
-
 };
 
-exports.signin = () => {};
+exports.signin = (req, res, next) => {
+  passport.authenticate("local", (error, user, info) => {
+    if (error) {
+      return res.status(500).json(error.message);
+    }
+    if (!user) {
+      return res.status(400).json(info.message);
+    }
+    req.logIn(user, err => {
+      if (err) {
+        return res.status(500).json(error.message);
+      }
+      res.json(user);
+    })
+  })(req, res, next);
+};
 
-exports.signout = () => {};
+exports.signout = (req, res) => {
+  res.clearCookie("next-cookie.sid");
+  req.logout();
+  res.json({
+    message: "You are now signout"
+  });
+};
 
-exports.checkAuth = () => {};
+exports.checkAuth = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/signin");
+};
